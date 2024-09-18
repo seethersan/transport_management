295c21522d095a7f9545ba65741364ac8bec3a1b
Juan Carlos Espinoza Zurita <carlos_jcez@hotmail.com>
2024-09-17 22:39:08 -0500

    add models

--- app/providers/models.py
+++ app/providers/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Provider(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=15)
+    language = models.CharField(max_length=10)
+    currency = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
--- app/service_areas/models.py
+++ app/service_areas/models.py
@@ -1,3 +1,11 @@
-from django.db import models
+from django.contrib.gis.db import models
+from providers.models import Provider
 
-# Create your models here.
+class ServiceArea(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    provider = models.ForeignKey(Provider, on_delete=models.CASCADE, related_name='service_areas')
+    geojson = models.PolygonField()
+
+    def __str__(self):
+        return f"{self.name} - {self.provider.name}"
