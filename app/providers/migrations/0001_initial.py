899401f48435b8417e9c30a995f29febd4b1a84f
Juan Carlos Espinoza Zurita <carlos_jcez@hotmail.com>
2024-09-18 01:02:23 -0500

    formatted

--- app/providers/migrations/0001_initial.py
+++ app/providers/migrations/0001_initial.py
@@ -7,19 +7,26 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
-            name='Provider',
+            name="Provider",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=100)),
-                ('email', models.EmailField(max_length=254, unique=True)),
-                ('phone_number', models.CharField(max_length=15)),
-                ('language', models.CharField(max_length=10)),
-                ('currency', models.CharField(max_length=10)),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("name", models.CharField(max_length=100)),
+                ("email", models.EmailField(max_length=254, unique=True)),
+                ("phone_number", models.CharField(max_length=15)),
+                ("language", models.CharField(max_length=10)),
+                ("currency", models.CharField(max_length=10)),
             ],
         ),
     ]
--- app/service_areas/migrations/0001_initial.py
+++ app/service_areas/migrations/0001_initial.py
@@ -10,18 +10,36 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('providers', '0001_initial'),
+        ("providers", "0001_initial"),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='ServiceArea',
+            name="ServiceArea",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=100)),
-                ('price', models.DecimalField(decimal_places=2, max_digits=10)),
-                ('geojson', django.contrib.gis.db.models.fields.PolygonField(srid=4326)),
-                ('provider', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='service_areas', to='providers.provider')),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("name", models.CharField(max_length=100)),
+                ("price", models.DecimalField(decimal_places=2, max_digits=10)),
+                (
+                    "geojson",
+                    django.contrib.gis.db.models.fields.PolygonField(srid=4326),
+                ),
+                (
+                    "provider",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="service_areas",
+                        to="providers.provider",
+                    ),
+                ),
             ],
         ),
     ]
