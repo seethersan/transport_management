7ff4eb3446082c812c408bb00f03313d289c873a
Juan Carlos Espinoza Zurita <carlos_jcez@hotmail.com>
2024-09-18 01:03:08 -0500

    formatted

--- app/service_areas/models.py
+++ app/service_areas/models.py
@@ -1,10 +1,13 @@
 from django.contrib.gis.db import models
 from providers.models import Provider
 
+
 class ServiceArea(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    provider = models.ForeignKey(Provider, on_delete=models.CASCADE, related_name='service_areas')
+    provider = models.ForeignKey(
+        Provider, on_delete=models.CASCADE, related_name="service_areas"
+    )
     geojson = models.PolygonField()
 
     def __str__(self):
